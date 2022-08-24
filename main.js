6ec3d148488c6358c39691bc98af926e2c8eee7f
Zauri Metreveli <72525534+Metrevelii@users.noreply.github.com>
2022-08-24 19:50:32 +0400

    Chaining Promise -- added neighbour country API

--- main.js
+++ main.js
@@ -56,9 +56,20 @@ const getJSON = function (url, errorMsg = 'Something went wrong') {
 // SIMPLIFIED VERSION
 
 const getCountryData = function (country) {
+    // Country 1
     fetch(`https://restcountries.com/v2/name/${country}`)
     .then((response) => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+        renderCountry(data[0]);
+        const neighbour = data[0].borders[0]
+
+        if (!neighbour) return;
+        // Country 2
+        return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour')); 
 };
 
 getCountryData('georgia');
+// getCountryData('austria');
